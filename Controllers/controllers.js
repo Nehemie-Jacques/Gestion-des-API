a4bc51e349298820284851732a3602974f1433b9
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-05-14 18:41:07 +0100

    creation

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -1,16 +1,37 @@
-import fs from 'fs';
-import path from 'path'; 
-import { randomUUID } from 'crypto';
+import fs from "fs";
+import path from "path";
+import { randomUUID } from "crypto";
 
-const dataPath = path.resolve('Database/base.json');
+const dataPath = path.resolve("Database/base.json");
 
 const dogsController = {
-    getAllDogs: (req, res) => {
-        fs.readFile(dataPath, 'utf-8', (err, data) => {
-            if (err) return res.status(500).send('Erreur de lecture du fichier');
-            res.json(JSON.parse(data));
-        });
-    }
+  getAllDogs: (req, res) => {
+    fs.readFile(dataPath, "utf-8", (err, data) => {
+      if (err) return res.status(500).send("Erreur de lecture du fichier");
+      res.json(JSON.parse(data));
+    });
+  },
+  createDog: (req, res) => {
+    const { name, age, breed } = req.body;
+    if (!name || !age || !breed)
+      return res.status(400).send("Tous les champs sont requis"); 
+
+    fs.readFile(dataPath, "utf-8", (err, data) => {
+      if (err) return res.status(500).send("Erreur de lecture du fichier"); 
+
+      const dogs = JSON.parse(data);
+
+      if (dogs.find((d) => d.name === name)) {
+        return res.status(400).send("Ce chien existe déjà"); 
+      }
+      dogs.push({ id: randomUUID(), name, age, breed });
+      fs.writeFile(dataPath, JSON.stringify(dogs), 'utf-8', (err) => {
+        if (err) return res.status(500).send('Erreur écriture');
+        res.status(201).send('Chien créé')
+      })
+
+    });
+  },
 }
 
 export default dogsController;
--- Routes/routes.js
+++ Routes/routes.js
@@ -4,8 +4,8 @@ import dogsController from '../Controllers/controllers.js';
 const router = Router();
 
 router.get('/', dogsController.getAllDogs);
-/* router.post('/create', dogsController.createDog);
-router.put('/update/:id', dogsController.updateDog);
+router.post('/create', dogsController.createDog);
+/*router.put('/update/:id', dogsController.updateDog);
 router.delete('/delete/:id', dogsController.deleteDog); */
 
 export default router;
