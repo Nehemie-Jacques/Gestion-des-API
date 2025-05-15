bba83c948a289e9052f116689daa3ac368525060
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-05-15 15:59:00 +0100

    update

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -5,17 +5,19 @@ import { randomUUID } from "crypto";
 const dataPath = path.resolve("Database/base.json");
 
 const dogsController = {
+
   getAllDogs: (req, res) => {
     fs.readFile(dataPath, "utf-8", (err, data) => {
       if (err) return res.status(500).send("Erreur de lecture du fichier");
       res.json(JSON.parse(data));
     });
   },
+
   createDog: (req, res) => {
     const { name, age, breed } = req.body;
-    if (!name || !age || !breed)
+    if (!name || !age || !breed) { 
       return res.status(400).send("Tous les champs sont requis"); 
-
+    }
     fs.readFile(dataPath, "utf-8", (err, data) => {
       if (err) return res.status(500).send("Erreur de lecture du fichier"); 
 
@@ -32,6 +34,23 @@ const dogsController = {
 
     });
   },
+
+  updateDog: (req, res) => {  
+    fs.readFile(dataPath, "utf-8", (err, data) => {
+      if (err) return res.status(500).send("Erreur de lecture du fichier");
+      const dogs = JSON.parse(data);
+      const dog = dogs.find(d => d.id === req.params.id);
+      if (!dog) return res.status(404).send("Chien non trouvé");
+
+      Object.assign(dog, req.body); // Met à jour les propriétés du chien avec les nouvelles valeurs
+      
+      fs.writeFile(dataPath, JSON.stringify(dogs), 'utf-8', (err) => {
+        if (err) return res.status(500).send('Erreur écriture');
+        res.send('Chien modifié');
+      });
+    });
+  }
+
 }
 
 export default dogsController;
--- Routes/routes.js
+++ Routes/routes.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/', dogsController.getAllDogs);
 router.post('/create', dogsController.createDog);
-/*router.put('/update/:id', dogsController.updateDog);
-router.delete('/delete/:id', dogsController.deleteDog); */
+router.put('/update/:id', dogsController.updateDog);
+/*router.delete('/delete/:id', dogsController.deleteDog); */
 
 export default router;
